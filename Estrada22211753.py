50738b096c79fa0131d0ac79d79ad1b77d15a381
Ian E. Estrada <ian.eec4@gmail.com>
2025-10-30 17:13:41 -0700

    Update Estrada22211753.py

--- Estrada22211753.py
+++ Estrada22211753.py
@@ -54,7 +54,7 @@ plt.plot(t,Fs2,'-',linewidth = 1, color =[0.114,0.208,0.341],label='Fs2(t):Caso'
 
 plt.grid(False) #Para poner una cuadricula en la grafica
 plt.xlim(0,10); plt.xticks(np.arange(0,11,1))
-plt.ylim(-0.6,1.4); plt.yticks(np.arange(-0.6,1.6,0.2))
+plt.ylim(-0.1,1.1); plt.yticks(np.arange(-0.1,1.2,0.1))
 plt.xlabel('t[s]')
 plt.ylabel('Fs[V]')
 plt.legend(bbox_to_anchor = (0.5,-0.2),loc = 'center', ncol = 3)
@@ -85,7 +85,7 @@ plt.plot(t,Fs2,'-',linewidth = 1, color =[0.114,0.208,0.341],label='Fs2(t):Caso'
 plt.plot(t,Fs3,':',linewidth = 1.5, color =[0.271,0.482,0.616],label='Fs3(t):Tratamiento')
 plt.grid(False) #Para poner una cuadricula en la grafica
 plt.xlim(0,10); plt.xticks(np.arange(0,11,1))
-plt.ylim(-0.6,1.4); plt.yticks(np.arange(-0.6,1.6,0.2))
+plt.ylim(-0.1,1.1); plt.yticks(np.arange(-0.1,1.2,0.1))
 plt.xlabel('t[s]')
 plt.ylabel('Fs[V]')
 plt.legend(bbox_to_anchor = (0.5,-0.2),loc = 'center', ncol = 3)
@@ -93,3 +93,4 @@ plt.show()
 fgl.tight_layout()
 fgl.savefig('Sistema Musculoesqueletico LC python.png',dpi=600,bbox_inches='tight')
 fgl.savefig('Sistema Musculoesqueletico LC python.pdf')
+
